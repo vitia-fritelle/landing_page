4d9422ccfbd138cd0a426267d622b8319b898d52
Vittorio Fritelle <vitorjunior@poli.ufrj.br>
2021-07-20 20:17:28 -0300

    Boas práticas e carregamento de imagem
    Realizei mudanças na localização dos arquivos js para views/components
    e fiz as adaptações necessárias. Também coloquei para fazer um
    carregamento prévio das imagens que aparecem da seção de descrição
    após a passagem pelo mouse.

--- src/views/components/background-email.js
+++ src/views/components/background-email.js
@@ -5,7 +5,6 @@ img.src=document.getElementById("fundo_email").src;
 section.style.height=`${3*(img.height)}px`;
 //rep();
 //window.onresize = rep(); 
-
 function rep(){
     let N = Math.floor(window.innerWidth/img.width);
     section.style.width=`${N*(img.width)}px`;
